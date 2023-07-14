ebcc0e9090b5251b9bbb886210c6293160fdae73
samadon1 <calvinsamueldonkor@gmail.com>
2023-07-14 09:57:48 +0000

    remove cd signtalk

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ set -o errexit  # exit on error
 
 # source env/scripts/activate
 pip install -r requirements.txt
-cd signtalkval
+# cd signtalkval
 python manage.py collectstatic --no-input
 python manage.py migrate
