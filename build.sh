4b77f740ddb6eb9c4b913e888b389de93e532d1b
Samuel Donkor <56901167+samadon1@users.noreply.github.com>
2023-07-24 22:30:38 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-set -o errexit  # exit on error
-
-# source env/scripts/activate
-pip install -r requirements.txt
-cd signtalkval
-python manage.py collectstatic --no-input
-python manage.py migrate
-cd signtalkval
